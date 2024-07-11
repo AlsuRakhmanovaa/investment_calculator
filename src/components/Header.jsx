1221bf6065c5078a1f0bdd3f89c6846be0ae5618
Alsu <rakhmanova_2003@inbox.ru>
2024-07-11 14:40:16 +0300

    passing the user input to the Result component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import image from '../assets/investment-calculator-logo.png'
+import image from '../assets/logo-money.png'
 
 export default function Header() {
     return <header id='header'>
-        <img src={image} alt="Logo money bag" />
+        <img src={image} alt="Logo money" />
         <h1>Investment Calculator</h1>
     </header>
 }
